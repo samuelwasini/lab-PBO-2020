df3bc2ff0cdd4aca88ca43f39843eb6d8d9d712c
Samuyel Wasini <samuelwasini12@gmail.com>
2020-11-13 02:09:14 +0700

    Pertemuan 3

---
+++ Pertemuan3/Manajer.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class Manajer here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Manajer extends Pegawai
+{ 
+  private String departemen;
+  
+  public Manajer(String nama, double gaji, String departemen)
+  {
+    Pegawai p = new Pegawai(nama, gaji);
+    this.departemen = departemen;
+   }
+  
+  public void setDepartemen(String departemen)
+  {
+    this.departemen = departemen;
+  }
+  
+  public String getDepartemen()
+  {
+   return this.departemen;
+  }
+}
+
+
---
+++ Pertemuan3/Pegawai.java
@@ -0,0 +1,40 @@
+
+/**
+ * Write a description of class Pegawai here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Pegawai
+{
+private String nama; private double gaji;
+
+public Pegawai(String nama, double gaji)
+{
+this.nama = nama;
+this.gaji = gaji;
+}
+
+public void setNama(String nama)
+{
+this.nama = nama;
+}
+
+public String getNama()
+{
+return this.nama;
+}
+
+public void setGaji(double gaji)
+{
+this.gaji = gaji;
+}
+
+public double getGaji()
+{
+return this.gaji;
+} 
+
+}
+
+
