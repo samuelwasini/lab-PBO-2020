99e8c2b08658391433f953bd29581f6a5a6f4ef8
Samuyel Wasini <samuelwasini12@gmail.com>
2020-11-13 01:59:29 +0700

    membuat Code Pertemuan 2

---
+++ prak2/CopyArray5.java
@@ -0,0 +1,12 @@
+public class CopyArray5 {
+public static void main(String args[]) {
+     String str1 = "Informatika Universitas Syiah Kuala"; 
+     System.out.println(str1.startsWith("Inf")); 
+     System.out.println(str1.endsWith("kuala")); 
+     System.out.println(str1.startsWith("Inf",1)); 
+     System.out.println(str1.startsWith("Inf",2));
+     System.out.println(str1.startsWith("Inf",3));
+    }	
+}
+ /*Program di atas bertujuan untuk menguji awalan atau 
+  akhir suatu string berntebet*/
